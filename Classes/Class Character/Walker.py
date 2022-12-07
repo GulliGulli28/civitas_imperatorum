1bdf454f784629892fe5dd1690881d2120d36223
qlemeur <quentin.le_meur@insa-cvl.fr>
2022-12-07 10:58:07 +0000

    update move

--- Classes/Class Character/Walker.py
+++ Classes/Class Character/Walker.py
@@ -36,6 +36,11 @@ class Walker(Character):
         if self.mainBuilding.getRange()<self.distanceToBuilding:#If the character is too far from his main building
             self.outOfRange = True
         self.updateDistanceToBuilding(self)
+        if not self.outOfRange:
+            if (self.positionX, self.positionY) in self.travelHistory:#if the character has already passed through this position then we reduce the travelHistory from iteration 0 to the iteration in which he has already passed
+                del self.travelHistory[self.travelHistory.index((self.positionX,self.positionY)):len(self.travelHistory)]
+            else :#else we add the position to the travelHistory
+                self.travelHistory.append((self.positionX,self.positionY))#add the position to the travelHistory
 
     def updateDistanceToBuilding(self):
         BposX = self.mainBuilding.getPositionX()#get the Building posX
