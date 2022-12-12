27e51bf04469bc0a9ab91e15ebc7f8839788faa6
Guillaume Lorin <guillaume.lorin@insa-cvl.fr>
2022-12-12 16:53:26 +0100

    retour des listes de sprites

--- main2.py
+++ main2.py
@@ -27,3 +27,38 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+Liste_Batiment = []
+Liste_Charactère =[]
+
+liste_sprites_NE_resident=[]
+liste_sprites_SW_resident=[]
+liste_sprites_NW_resident=[]
+liste_sprites_SE_resident=[]
+
+liste_sprites_NE_walker=[]
+liste_sprites_SW_walker=[]
+liste_sprites_NW_walker=[]
+liste_sprites_SE_walker=[]
+
+liste_sprites_NE_worker=[]
+liste_sprites_SW_worker=[]
+liste_sprites_NW_worker=[]
+liste_sprites_SE_worker=[]
+
+for i in range(0,12):
+    liste_sprites_NE_resident.append("C3_sprites/C3/0_fired_00001"+"png")
+    liste_sprites_SW_resident.append("C3_sprites/C3/0_fired_00001"+"png")
+    liste_sprites_NW_resident.append("C3_sprites/C3/0_fired_00001"+"png")
+    liste_sprites_SE_resident.append("C3_sprites/C3/0_fired_00001"+"png")
+
+    liste_sprites_NE_walker.append("C3_sprites/C3/citizen02_00"+str(615+i*8)+".png")
+    liste_sprites_SW_walker.append("C3_sprites/C3/citizen02_00"+str(619+i*8)+".png")
+    liste_sprites_NW_walker.append("C3_sprites/C3/citizen02_00"+str(621+i*8)+".png")
+    liste_sprites_SE_walker.append("C3_sprites/C3/citizen02_00"+str(617+i*8)+".png")
+
+    liste_sprites_NE_worker.append("C3_sprites/C3/0_fired_00001"+"png")
+    liste_sprites_SW_worker.append("C3_sprites/C3/0_fired_00001"+"png")
+    liste_sprites_NW_worker.append("C3_sprites/C3/0_fired_00001"+"png")
+    liste_sprites_SE_worker.append("C3_sprites/C3/0_fired_00001"+"png")
