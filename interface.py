0874d0599ecb0b9c95f5495b5b2bbc8f0119e20d
Ngo Minh Phong <minh_phong.ngo@insa-cvl.fr>
2022-12-12 15:57:53 +0100

    housing and building class added

--- interface.py
+++ interface.py
@@ -1,16 +1,21 @@
 import pygame
+from housing import Building,Test_Building,List_Building,place_building
 
 bg = pygame.image.load("C3_sprites/C3/Land1a_00002.png")
-bg_width = bg.get_width()/2
-bg_height = bg.get_height()
 
+#remplacer votre background en isometric
 def draw_background(screen,width,height):
-    screen.fill((225,225,225))
+    screen.fill("black")
     i = 0
     j = 0
-    length_x = int(width/bg_width)
-    length_y = int(height/bg_height)
+    length_x = int(width/50)
+    length_y = int(height/50)
     for i in range(length_y):
         for j in range(length_x):
-            screen.blit(bg,(int((i-j)*bg_width),(int((i+j)*bg_height/2))))
+            pygame.draw.rect(screen,"Blue",(i*50,j*50,bg_width,bg_height),1)
 
+def draw_house(screen):
+    place_building(screen,1,5)
+    place_building(screen,3,4)
+    for Building in List_Building:
+        screen.blit(Building.image,(Building.x*50,Building.y*50))
