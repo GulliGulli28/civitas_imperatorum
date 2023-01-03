7df5c809e9b3f3b0edaf2736c4d866649d652ecd
qlemeur <quentin.le_meur@insa-cvl.fr>
2023-01-03 18:13:25 +0000

    graphe pour pathfinding 1.1

--- Classes/Class_Building/mapBuilding.py
+++ Classes/Class_Building/mapBuilding.py
@@ -21,7 +21,7 @@ class mapBuilding():
                     self.map[newbuild.positionX + y][newbuild.positionY + i] = p
                 self.map[newbuild.positionX + i][newbuild.positionY + i] = p
 
-    def get_direction(self, positionX, positionY):
+    def get_direction(self, positionX, positionY):  # TODO modifier pour retourner (x, y)
         """
         Method used by a Character to get the possible future direction
 
@@ -60,23 +60,38 @@ class mapBuilding():
             direct = self.get_direction(self, x, y)
             if len(direct) > 1:
                 for i in range(1, direct):
-                    distance = self.calcule_distance(pos, i)  # TODO résoudre ce mystère
-                    self.add_dist2graph(self, pos, distance)
+                    (pos2, distance) = self.calculate_distance(pos, i)
+                    self.add_dist2graph(self, pos, pos2, distance)
 
-    def calcule_distance(self, pos, i):
+    def calculate_distance(self, pos, i):  # TODO vérifier que c'est correct
+        """
+        Method used to calculate the distance between 2 intersections on the road
+
+        Args:
+            pos (int,int): Position of the road just added.
+            i (int,int): The value needed to be sum with the position to get the future position
+
+        Returns:
+            pos2 (int, int): Position of the second intersection
+            distance (int): The distance calculate
+        :param pos:
+        :param i:
+        :return:
+        """
         distance = 0
         stop = False
-        while stop == False:
+        while not stop:
             old_pos = pos
             pos += i
             distance += 1
             (x, y) = pos
-            if len(self.get_direction(x,y))==2:
-                #normal
-            elif len(self.get_direction(x,y))!=2:
-                #fin de route
+            direct = self.get_direction(x, y)
+            if len(direct) == 2:
+                direct.remove(old_pos - pos)
+                i = direct[1]
+            elif len(self.get_direction(x, y)) != 2:
+                pos2 = pos
+                stop = True
+        return pos2, distance
 
-        return distance
-
-
-
+    def add_dist2graph(self, pos1, pos2, distance):
