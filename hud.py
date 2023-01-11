76c43deb1974d7e619dc7d6abcc50522dbbaf6e8
Ngo Minh Phong <minh_phong.ngo@insa-cvl.fr>
2023-01-11 14:07:52 +0100

    placing building no longer overlay hud

--- hud.py
+++ hud.py
@@ -1,5 +1,5 @@
 import pygame as pg
-
+from settings import HUD_WIDTH
 
 class Hud:
 
@@ -17,12 +17,12 @@ class Hud:
 
         # right hud
         self.build_surface = pg.Surface((width * 0.106, height))
-        self.build_rect = self.build_surface.get_rect(topleft=(self.width * 0.84, self.height * 0.74))
+        self.build_rect = self.build_surface.get_rect(topleft=(self.width - HUD_WIDTH, self.height * 0))
         self.build_surface.fill(self.hud_colour)
 
         # select hud
         self.select_surface = pg.Surface((width * 0.3, height * 0.2), pg.SRCALPHA)
-        self.select_rect = self.select_surface.get_rect(topleft=(self.width * 0.35, self.height * 0.79))
+        self.select_rect = self.select_surface.get_rect(topleft=(self.width - HUD_WIDTH, self.height * 0.79))
         self.select_surface.fill(self.hud_colour)
 
 
@@ -33,7 +33,8 @@ class Hud:
 
     def draw(self, screen):
         images = self.images
-        right_hud_width = images["right_hud"].get_width()
+        hud_width = images["right_hud"].get_width()
+        print(HUD_WIDTH) 
         icon_1_width, icon_1_height = images["icon_1"].get_width(), images["icon_1"].get_height()
         # resources hud
         screen.blit(self.resources_surface, (0, 0))
@@ -44,9 +45,9 @@ class Hud:
         # select hud
         # screen.blit(self.select_surface, (self.width * 0.35, self.height * 0.79))
         
-        screen.blit(images["right_hud"], (self.width - right_hud_width, self.height * 0.02))
+        screen.blit(images["right_hud"], (self.width - HUD_WIDTH, self.height * 0.02))
         screen.blit(images["icon_1"], (self.width - icon_1_width, self.height - icon_1_height))
-        screen.blit(images["overlays_case"], (self.width - right_hud_width + 4.5, self.resources_surface.get_height() + 3.5))
+        screen.blit(images["overlays_case"], (self.width - HUD_WIDTH + 4.5, self.resources_surface.get_height() + 3.5))
 
 
         #affichage icons
@@ -56,28 +57,28 @@ class Hud:
         # OVERLAY TEXT
         font = pg.font.Font(None, 34)
         overlays_text = font.render("Overlays", True, (0, 0, 0))
-        screen.blit(overlays_text, (self.width-right_hud_width+10, 21))
+        screen.blit(overlays_text, (self.width-HUD_WIDTH+10, 21))
 
     def build_hud(self):
         images = self.images
         icons = self.icons
-        right_hud_width = images["right_hud"].get_width()
+        HUD_WIDTH = images["right_hud"].get_width()
         #button
         tiles = []
         for name, image in icons.items():
             name = name
             if name == "housing":
-                pos = [self.width - right_hud_width + 13.5, 295]
+                pos = [self.width - HUD_WIDTH + 13.5, 295]
             elif name == "clear":
-                pos = [self.width - right_hud_width + 63, 295]
+                pos = [self.width - HUD_WIDTH + 63, 295]
             elif name == "road":
-                pos = [self.width - right_hud_width + 113, 295]
+                pos = [self.width - HUD_WIDTH + 113, 295]
             elif name == "water":
-                pos = [self.width - right_hud_width + 13.5, 330]
+                pos = [self.width - HUD_WIDTH + 13.5, 330]
             elif name == "government":
-                pos = [self.width - right_hud_width + 63.5, 330]
+                pos = [self.width - HUD_WIDTH + 63.5, 330]
             elif name == "security":
-                pos = [self.width - right_hud_width + 113.5, 330]
+                pos = [self.width - HUD_WIDTH + 113.5, 330]
             img_tmp = image.copy()
             rect    = img_tmp.get_rect(topleft=pos)
             tiles.append(
