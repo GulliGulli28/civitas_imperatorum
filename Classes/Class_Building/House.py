ff9afb4280bd1aaf1a7cd69fba9f69f2c2d1b2b7
Guillaume Lorin <guillaume.lorin@insa-cvl.fr>
2023-01-26 17:02:14 +0100

    allumer le feu

--- Classes/Class_Building/House.py
+++ Classes/Class_Building/House.py
@@ -1,8 +1,15 @@
 from Classes.Class_Building.Building import Building
-
+import random
+import time
 
 class House(Building):
 
     def __init__(self, positionX, positionY, size, capacity, price, idi):
         super().__init__(positionX, positionY, size, capacity, price, idi)
         self.name = "housing"
+
+
+
+
+
+
--- Classes/logic_main.py
+++ Classes/logic_main.py
@@ -4,15 +4,19 @@ from Classes.Class_Building import Building
 from Classes.Class_Building.House import House
 from Classes.Class_Building.Road import Road
 from Classes.Class_Character.Migrant import Migrant
+import time
 
 
-class logic_main:
+class Logic_main:
     def __init__(self, resident, money):
         self.resident = resident
         self.money = money
 
     map_build = mapBuilding()
     map_char = mapCharacter()
+    drhouse = House(1, 2, 1, 1, 1, 1)
+    drhouse.fire_risk = 0.01
+
     for i in range(0, 40):
         road = Road(0, i)
         map_build.add_build(road)
@@ -22,6 +26,9 @@ class logic_main:
     migr = Migrant(0, 0, (0, 1))
     map_char.add_character(migr)
     while 1:
+        drhouse.risque_feu()
+        drhouse.update_risk()
+        print(drhouse.is_on_fire)
         for char in map_char.list:
             if isinstance(char, Migrant):
                 if char.move():
@@ -31,6 +38,7 @@ class logic_main:
             else:
                 char.move()
         print(map_char.list)
+        time.sleep(1)
     # while 1:
     #     for build_list in map_build.map:
     #         for build in build_list:
